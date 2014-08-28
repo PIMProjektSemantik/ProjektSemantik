22d33459f141b9423c2875678a94221a7395c1ff
Stefan Walle <Stefan_Walle@gmx.de>
2014-08-28 09:49:34 +0200

    - Dummy Datenbankklasse für Geräteanzeige hinzugefügt

---
+++ JPA-Project/src/main/java/de/htwsaarland/scenario/ScenarioTreeStepDBOWLDevices.java
@@ -0,0 +1,47 @@
+package de.htwsaarland.scenario;
+
+/**
+ * Schritt für Geräte-Abfrage aus Datenbank und Ontologie.
+ * 
+ * Besitzt nur einen Folgeschritt.
+ * 
+ * @author Stefan
+ *
+ */
+public class ScenarioTreeStepDBOWLDevices extends ScenarioTreeStepSimpleDatabaseRequest {
+
+	/**
+	 * Konstruiert den Geräte-Datenbankschritt mit seinem Namen
+	 * 
+	 * @param name
+	 */
+	public ScenarioTreeStepDBOWLDevices(String name, String help) {
+		super(name, help);
+		
+	}
+
+	
+	@Override
+	public void addFollowUpStep(ScenarioTreeStep stepToAdd) {
+		
+		if(this.getFollowUpSteps().length >= 1) {
+			throw new RuntimeException("ScenarioTreeStepBDOWLAccessory: Nur ein Folgeschritt möglich");
+		}
+		
+		super.addFollowUpStep(stepToAdd);
+	};
+	
+	/**
+	 * Liefert den einzigen Folgeschritt, den dieser Schritt haben kann.
+	 */
+	@Override
+	public ScenarioTreeStep getNextStep() {
+		
+		if(this.getFollowUpSteps().length == 0){
+			throw new RuntimeException("ScenarioTreeStepBDOWLAccessory: Kein Folgeschritt gesetzt!");
+		}
+		
+		return this.getFollowUpStep(0);
+	}
+
+}
