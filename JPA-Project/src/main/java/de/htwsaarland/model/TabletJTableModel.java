4421c52093a2757a3745b7013338e3542df5a880
Stefan Walle <Stefan_Walle@gmx.de>
2014-08-31 12:28:00 +0200

    - Fehlende Tabellenspalte ergänzt in Tablettabelle

--- JPA-Project/src/main/java/de/htwsaarland/model/TabletJTableModel.java
+++ JPA-Project/src/main/java/de/htwsaarland/model/TabletJTableModel.java
@@ -14,10 +14,10 @@ public class TabletJTableModel extends AbstractTableModel {
 	private String query;
 	
 	// Spalten. Können statisch definiert werden, da dies im DAO auch so ist, das als Datenbankinterface dient.
-	private String[] columnNames = {"Hersteller", "Modell", "Betriebssystem", "Display", "Prozessor", "Speicher", "Funk", "Preis"};
+	private String[] columnNames = {"Prod.Nr", "Hersteller", "Modell", "Betriebssystem", "Display", "Prozessor", "Speicher", "Funk", "Preis"};
 	
 	@SuppressWarnings("rawtypes")
-	private Class[] classList = {String.class, String.class, String.class, Integer.class, String.class, String.class, String.class, String.class};
+	private Class[] classList = {Integer.class, String.class, String.class, String.class, Integer.class, String.class, String.class, String.class, String.class};
 	private Object[][] data;
 	
 	/**
@@ -53,14 +53,15 @@ public class TabletJTableModel extends AbstractTableModel {
 			
 			Tablet currentTablet = entries.get(i);
 			
-			data[i][0] = currentTablet.getHersteller();
-			data[i][1] = currentTablet.getModell();
-			data[i][2] = currentTablet.getBetriebssystem();
-			data[i][3] = currentTablet.getDisplaygroesse();
-			data[i][4] = currentTablet.getProzessor();
-			data[i][5] = currentTablet.getSpeicher() + " GB";
-			data[i][6] = currentTablet.getConnectionType();
-			data[i][7] = currentTablet.getPreis() + " €";
+			data[i][0] = currentTablet.getId();
+			data[i][1] = currentTablet.getHersteller();
+			data[i][2] = currentTablet.getModell();
+			data[i][3] = currentTablet.getBetriebssystem();
+			data[i][4] = currentTablet.getDisplaygroesse();
+			data[i][5] = currentTablet.getProzessor();
+			data[i][6] = currentTablet.getSpeicher() + " GB";
+			data[i][7] = currentTablet.getConnectionType();
+			data[i][8] = currentTablet.getPreis() + " €";
 			
 		}
 		
